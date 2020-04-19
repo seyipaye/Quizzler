31969f3f932270c15f5661d74098ac345fc16aa8
Seyi <seyipaye@gmail.com>
2020-04-19 08:31:40 +0100

    Connect interface, and display quiz with a 2D Array

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIImageView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greaer than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = quiz[0][0]
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct")
+        } else {
+            print("Wrong")
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+            questionLabel.text = quiz[questionNumber][0]
+        } else {
+            questionNumber = 0
+            questionLabel.text = quiz[0][0]
+        }
+    }
+    
 }
 
